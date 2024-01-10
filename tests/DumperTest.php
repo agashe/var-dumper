7beee49bb4dc53eae6868a48d06fd082fbfe6068
mohamed.yousef <engineer.mohamed.yossef@gmail.com>
2024-01-10 10:39:32 +0200

    testing done

--- tests/DumperTest.php
+++ tests/DumperTest.php
@@ -9,47 +9,75 @@ use VarDumper\Dumper;
 class DumperTest extends TestCase
 {
     /**
+     * Select output type and print the value according to.
+     * 
+     * @param string $outputType
+     * @return object
+     */
+    private function getDumpFunction($outputType)
+    {
+        $output = '';
+        $file = '';
+
+        switch ($outputType) {
+            case 'cli':
+                $output = Dumper::VAR_DUMPER_OUTPUT_CLI;
+                break;
+            case 'web':
+                $output = Dumper::VAR_DUMPER_OUTPUT_WEB;
+                break;
+            case 'file':
+            case 'json':
+                $output = Dumper::VAR_DUMPER_OUTPUT_FILE;
+                $file = __DIR__ . "/output/file." . ($outputType == 'json' ?
+                    'json' : 'txt');
+                break;
+        }
+
+        return function (...$variables) use ($output, $file) {
+            Dumper::dump($output, $variables, debug_backtrace(), $file);
+        };
+    }
+
+    /**
      * Loop throw set of lines and check if all are matches.
      * 
      * @param string $results
+     * @param string $validResults
      * @return bool
      */
-    private function checkResults($results)
+    private function checkResults($results, $validResults)
     {
-        $validResults = explode("\n", 
-            file_get_contents(__DIR__ . '/results.txt'));
-
+        $validResults = explode("\n", $validResults);
         $results = explode("\n", $results);
 
         foreach($results as $i => $line) {
+            // skip all variable values , since these values
+            // will always change
             if (strpos($line, 'DumperTest.php') !== false ||
-                strpos($line, 'results.txt') !== false ||
+                strpos($line, 'cli.txt') !== false ||
                 strpos($line, 'file =>') !== false ||
+                strpos($line, 'dump_') !== false ||
+                strpos($line, 'time') !== false ||
+                strpos($line, 'line') !== false ||
                 empty(trim($line))
             ) {
                 continue;
             }
 
+            // skip zend auto generated ids
             $results[$i] = preg_replace(
-                '~\#([0-9]*) \=\> \{~', 'N', $results[$i]
+                '~\#([0-9]*)~', 'N', $results[$i]
             );
 
             $validResults[$i] = preg_replace(
-                '~\#([0-9]*) \=\> \{~', 'N', $validResults[$i]
-            );
-
-            $results[$i] = preg_replace(
-                '~\#([0-9]*) \(.*?\) \=\> \{~', 'N', $results[$i]
-            );
-
-            $validResults[$i] = preg_replace(
-                '~\#([0-9]*) \(.*?\) \=\> \{~', 'N', $validResults[$i]
+                '~\#([0-9]*)~', 'N', $validResults[$i]
             );
 
             if (trim($results[$i]) != trim($validResults[$i])) {
-                // print $i . PHP_EOL;
-                // print trim($results[$i]) . PHP_EOL;
-                // print trim($validResults[$i]) . PHP_EOL;
+                print $i . PHP_EOL;
+                print trim($results[$i]) . PHP_EOL;
+                print trim($validResults[$i]) . PHP_EOL;
                 return false;
             }
         }
@@ -58,34 +86,36 @@ class DumperTest extends TestCase
     }
 
     /**
-     * Test dump data.
+     * Run dumper method on a set of data.
      *
-     * @runInSeparateProcess
+     * @param object $dumper
      * @return void
      */
-    public function testDump()
+    private function runDumper($dumper)
     {
-        ob_start();
-
         // basic
         $hello = "world";
 
-        dump('ahmed', 5.6, false, $hello, NULL);
-        dump("/^([A-Za-z1-9])$/");
-        dump(serialize([1, 2, 3]));
-        dump("http://www.helloworld.com");
-        dump("test@example.com");
-        dump('20-5-1995');
-        dump(fopen(__DIR__ . '/results.txt', 'r'));
+        $dumper('ahmed', 5.6, false, $hello, NULL);
+        $dumper("/^([A-Za-z1-9])$/");
+        $dumper(serialize([1, 2, 3]));
+        $dumper("http://www.helloworld.com");
+        $dumper("test@example.com");
+        $dumper('20-5-1995');
+        $dumper(fopen(__DIR__ . '/results/cli.txt', 'r'));
 
         // arrays
-        dump(array(1, 2, 3));
+        $dumper(array(1, 2, 3));
 
         // objects
-        dump(new \StdClass());
+        $dumper(new \StdClass());
 
-        $d = DateTime::createFromFormat('Y-m-d h:i:s', '1995-5-20 12:28:14');
-        dump($d);
+        $d = DateTime::createFromFormat(
+            'Y-m-d h:i:s',
+            '1995-5-20 12:28:14'
+        );
+
+        $dumper($d);
 
         $bar = new Bar();
         $foo = new Foo();
@@ -110,10 +140,10 @@ class DumperTest extends TestCase
 
         $foo->bar = $bar;
 
-        dump($foo);
+        $dumper($foo);
 
         // test anonymous class
-        dump(
+        $dumper(
             new class
             {
                 public $name;
@@ -130,14 +160,14 @@ class DumperTest extends TestCase
             return $name;
         };
 
-        dump($closure);
-        dump(function () {});
+        $dumper($closure);
+        $dumper(function () {});
 
         // test enum
-        dump((Week::Saturday));
+        $dumper((Week::Saturday));
 
         // test array
-        dump([
+        $dumper([
             'a' => 'apple',
             'b' => 'banana',
             'c' => [1, 2, [1, 2, 3]],
@@ -147,10 +177,88 @@ class DumperTest extends TestCase
 
         // test long text
         // THIS LINE IS EXCEPTION FOR THE 80 CHARACTERS RULE :(
-        dump("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris viverra at est sed vestibulum. Ut ultricies urna et bibendum sollicitudin. Maecenas suscipit bibendum ante convallis dignissim. Nulla facilisi. Sed mollis eget purus eget finibus.Donec convallis risus sit amet dapibus vehicula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum vel aliquet ante. Suspendisse vitae neque non nulla viverra blandit at at diam. Phasellus imperdiet quis lacus sed facilisis. Mauris lacinia arcu lorem, quis commodo arcu fringilla in. ");
+        $dumper("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris viverra at est sed vestibulum. Ut ultricies urna et bibendum sollicitudin. Maecenas suscipit bibendum ante convallis dignissim. Nulla facilisi. Sed mollis eget purus eget finibus.Donec convallis risus sit amet dapibus vehicula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum vel aliquet ante. Suspendisse vitae neque non nulla viverra blandit at at diam. Phasellus imperdiet quis lacus sed facilisis. Mauris lacinia arcu lorem, quis commodo arcu fringilla in. ");
+    }
+
+    /**
+     * Test dump data to CLI output.
+     *
+     * @runInSeparateProcess
+     * @return void
+     */
+    public function testDumpCli()
+    {
+        ob_start();
+
+        $this->runDumper($this->getDumpFunction('cli'));
 
         $this->assertTrue(
-            $this->checkResults(ob_get_clean())
+            $this->checkResults(
+                ob_get_clean(), 
+                file_get_contents(__DIR__ . "/results/cli.txt")
+            )
+        );
+    }
+
+    /**
+     * Test dump data to Web output.
+     *
+     * @runInSeparateProcess
+     * @return void
+     */
+    public function testDumpWeb()
+    {
+        ob_start();
+
+        $this->runDumper($this->getDumpFunction('web'));
+
+        $this->assertTrue(
+            $this->checkResults(
+                ob_get_clean(), 
+                file_get_contents(__DIR__ . "/results/web.html")
+            )
+        );
+    }
+
+    /**
+     * Test dump data to a file.
+     *
+     * @runInSeparateProcess
+     * @return void
+     */
+    public function testDumpFile()
+    {
+        // clear output file
+        file_put_contents(__DIR__ . "/output/file.txt", "");
+
+        $this->runDumper($this->getDumpFunction('file'));
+
+        $this->assertTrue(
+            $this->checkResults(
+                file_get_contents(__DIR__ . "/output/file.txt"),
+                file_get_contents(__DIR__ . "/results/file.txt")
+            )
+        );
+    }
+
+    /**
+     * Test dump data to a json file.
+     *
+     * @runInSeparateProcess
+     * @return void
+     */
+    public function testDumpJsonFile()
+    {
+        // clear output file
+        file_put_contents(__DIR__ . "/output/file.json", "");
+        
+        $this->runDumper($this->getDumpFunction('json'));
+
+        $this->assertTrue(
+            $this->checkResults(
+                file_get_contents(__DIR__ . "/output/file.json"),
+                file_get_contents(__DIR__ . "/results/file.json")
+            )
         );
     }
 
@@ -164,7 +272,7 @@ class DumperTest extends TestCase
     {
         $this->expectException(\InvalidArgumentException::class);
 
-        \VarDumper\Dumper::dump('', '', '');
+        Dumper::dump('', '', '');
     }
 
     /**
